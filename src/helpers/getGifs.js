eba4186918e84e41359a1a5fdf250f1ab8a9ef0f
Saul Fraire Alvarado <saul.fraire@outlook.com>
2020-06-29 19:48:31 -0500

    fetch corregido a https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=3BJTMUhQL5oWqjQ9u5zClYc36Qkw6hao`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=3BJTMUhQL5oWqjQ9u5zClYc36Qkw6hao`;
     const res = await fetch(url);
     const { data } = await res.json();
     const gifs = data.map(d => {
