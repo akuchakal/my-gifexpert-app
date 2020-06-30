6024eebc1c63c3e1d0e54011bf89efd1bcea42cc
Saul Fraire Alvarado <saul.fraire@outlook.com>
2020-06-29 19:39:44 -0500

    Docs para git

---
+++ docs/precache-manifest.65bd8be70b02961f5470c49eec1b895a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc98161a0c8f195380691ec3d1eb849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "834b4f57231fc483a4c0",
+    "url": "/static/css/main.05401b6d.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834b4f57231fc483a4c0",
+    "url": "/static/js/main.967c35fa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
